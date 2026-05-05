86ae801bd7fa2f71af7bf6f3d0bbd13f33719beb
Allen <Allenpmraz@gmail.com>
2026-05-05 16:42:51 -0400

    Added some data structure to Character

--- RPGPreactice/RPGPreactice/Models/Character.cs
+++ RPGPreactice/RPGPreactice/Models/Character.cs
@@ -1,7 +1,15 @@
-﻿namespace RPGPreactice.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RPGPreactice.Models
 {
     public class Character
     {
+        public int CharacterId { get; set; }
+        [Required]
+        public string Name { get; set; } = string.Empty;
+
+        public int Level { get; set; }
+
 
     }
 }
